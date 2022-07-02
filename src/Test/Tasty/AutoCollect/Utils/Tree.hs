83fc90542aecc697ef2a79a59468577773508f03
Brandon Chinn <brandonchinn178@gmail.com>
2022-07-02 11:17:27 -0700

    Break out toTreeMap

--- src/Test/Tasty/AutoCollect/Utils/Tree.hs
+++ src/Test/Tasty/AutoCollect/Utils/Tree.hs
@@ -17,7 +17,10 @@ data Tree k v = Tree
   }
   deriving (Show, Eq)
 
-data TreeMap k v = TreeMap (Maybe v) (Map k (TreeMap k v))
+data TreeMap k v = TreeMap
+  { value' :: Maybe v
+  , children' :: Map k (TreeMap k v)
+  }
 
 {- |
 Convert the given list of values into a tree.
@@ -70,18 +73,68 @@ Tree
 @
 -}
 toTree :: Ord k => [([k], v)] -> Tree k v
-toTree = fromTreeMap Seq.empty . foldr insertTreeMap emptyTreeMap
+toTree = fromTreeMap Seq.empty . fromList
   where
-    emptyTreeMap = TreeMap Nothing Map.empty
-
-    insertTreeMap (ks, v) (TreeMap mVal children) =
-      case ks of
-        [] -> TreeMap (Just v) children
-        k : ks' -> TreeMap mVal $ Map.alter (Just . insertTreeMap (ks', v) . fromMaybe emptyTreeMap) k children
-
     fromTreeMap path (TreeMap mVal children) =
       Tree
         { fullPath = toList path
         , value = mVal
         , subTrees = Map.elems $ Map.mapWithKey (fromTreeMap . (path |>)) children
         }
+
+{- |
+Convert the given list of values into a 'TreeMap'.
+
+For example,
+@
+fromList [[A, B, C], [A, B], [A, C, D], [Z]]
+@
+would become
+@
+TreeMap
+  { value = Nothing
+  , children = Map.fromList
+      [ ("A", TreeMap
+          { value = Nothing
+          , children = Map.fromList
+              [ ("B", TreeMap
+                  { value = Just ...
+                  , children = Map.fromList
+                      ("C", [ TreeMap
+                          { value = Just ...
+                          , children = Map.empty
+                          }
+                      ])
+                  })
+              , ("C", TreeMap
+                  { value = Nothing
+                  , children = Map.fromList
+                      [ ("D", TreeMap
+                          { value = Just ...
+                          , children = Map.empty
+                          })
+                      ]
+                  })
+              ]
+          })
+    , ("Z", TreeMap
+        { value = Just ...
+        , children = Map.empty
+        })
+    ]
+  }
+@
+-}
+fromList :: Ord k => [([k], v)] -> TreeMap k v
+fromList = foldr (uncurry insert) empty
+
+empty :: TreeMap k v
+empty = TreeMap Nothing Map.empty
+
+insert :: Ord k => [k] -> v -> TreeMap k v -> TreeMap k v
+insert originalKeys v = go originalKeys
+  where
+    go ks treeMap =
+      case ks of
+        [] -> treeMap{value' = Just v}
+        k : ks' -> treeMap{children' = Map.alter (Just . go ks' . fromMaybe empty) k (children' treeMap)}
