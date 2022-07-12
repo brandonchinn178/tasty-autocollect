43f2e4a842e41e5b0cea0930ec7ce28798a0e6f4
Brandon Chinn <brandonchinn178@gmail.com>
2022-07-11 19:20:42 -0700

    Support no prelude

--- src/Test/Tasty/AutoCollect/ExternalNames.hs
+++ src/Test/Tasty/AutoCollect/ExternalNames.hs
@@ -15,12 +15,14 @@ import Test.Tasty.Ext.Todo (testTreeTodo)
 data ExternalNames = ExternalNames
   { name_TestTree :: Name
   , name_testTreeTodo :: Name
+  , name_concat :: Name
   }
 
 loadExternalNames :: HscEnv -> IO ExternalNames
 loadExternalNames env = do
   name_TestTree <- loadName ''TestTree
   name_testTreeTodo <- loadName 'testTreeTodo
+  name_concat <- loadName 'concat
   pure ExternalNames{..}
   where
     loadName name =
