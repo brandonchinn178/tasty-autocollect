cd89d33f01d6aa4c649ce0852ad03cb39e8400e7
Brandon Chinn <brandonchinn178@gmail.com>
2024-05-15 22:34:30 -0700

    Strip all newlines at end of autocollect errors

--- src/Test/Tasty/AutoCollect/Error.hs
+++ src/Test/Tasty/AutoCollect/Error.hs
@@ -2,11 +2,13 @@ module Test.Tasty.AutoCollect.Error (
   autocollectError,
 ) where
 
+import Data.List (dropWhileEnd)
+
 import Test.Tasty.AutoCollect.GHC
 
 autocollectError :: String -> a
 autocollectError msg =
-  pgmError . unlines $
+  pgmError . dropWhileEnd (== '\n') . unlines $
     [ ""
     , "******************** tasty-autocollect failure ********************"
     , msg
