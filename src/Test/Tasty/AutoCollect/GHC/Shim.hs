566560438ff158257fa018d823d8a5b1d2e2b2d2
Brandon Chinn <brandonchinn178@gmail.com>
2024-05-15 21:27:39 -0700

    Drop support for GHC 9.4

--- src/Test/Tasty/AutoCollect/GHC/Shim.hs
+++ src/Test/Tasty/AutoCollect/GHC/Shim.hs
@@ -4,9 +4,7 @@ module Test.Tasty.AutoCollect.GHC.Shim (module X) where
 
 -- GHC-specific shims
 import Test.Tasty.AutoCollect.GHC.Shim_Common as X
-#if __GLASGOW_HASKELL__ == 904
-import Test.Tasty.AutoCollect.GHC.Shim_9_4 as X
-#elif __GLASGOW_HASKELL__ == 906
+#if __GLASGOW_HASKELL__ == 906
 import Test.Tasty.AutoCollect.GHC.Shim_9_6 as X
 #elif __GLASGOW_HASKELL__ == 908
 import Test.Tasty.AutoCollect.GHC.Shim_9_8 as X
